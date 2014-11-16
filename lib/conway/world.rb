6e2afdf794608dfadb2fb9cef7eece5c363faa49
Max Holder <mxhold@gmail.com>
2014-11-15 22:17:51 -0500

    Add World#next

--- lib/conway/world.rb
+++ lib/conway/world.rb
@@ -1,7 +1,7 @@
 module Conway
   class World
-    def initialize(cell_string)
-      @cell_grid = cells_from_string(cell_string)
+    def initialize(cell_string, cell_grid: nil)
+      @cell_grid = cell_grid || cells_from_string(cell_string)
     end
 
     def to_s
@@ -12,17 +12,55 @@ module Conway
       end.join("\n")
     end
 
+    def next
+      self.class.new(nil, cell_grid: next_cell_grid)
+    end
+
     private
 
     attr_reader :cell_grid
 
+    def next_cell_grid
+      cell_grid.map.with_index do |row, row_index|
+        row.map.with_index do |cell, cell_index|
+          next_cell(cell, row_index, cell_index)
+        end
+      end
+    end
+
     def cells_from_string(cell_string)
-      rows = cell_string.split("\n")
-      rows.map do |row|
+      cell_string.split("\n").map do |row|
         row.each_char.map do |char|
           Cell.new(alive: char == '1')
         end
       end
+    end
+
+    def next_cell(cell, row_index, cell_index)
+      cell.next(alive_neighbors: alive_neighbors(row_index, cell_index))
+    end
+
+    def alive_neighbors(row_index, cell_index)
+      neighbors(row_index, cell_index).select(&:alive?).size
+    end
+
+    def neighbors(row_index, cell_index)
+      [
+        [-1, -1],
+        [-1, 0],
+        [-1, 1],
+        [0, -1],
+        [0, 1],
+        [1, -1],
+        [1, 0],
+        [1, 1],
+      ].map do |x_shift, y_shift|
+        x = row_index +  x_shift
+        y = cell_index + y_shift
+        if (x >= 0) && (x >= 0)
+          cell_grid.fetch(x, {})[y]
+        end
+      end.compact
     end
   end
 end
--- spec/world_spec.rb
+++ spec/world_spec.rb
@@ -1,9 +1,19 @@
 require 'spec_helper'
 
 describe Conway::World do
+  subject do
+    described_class.new("010\n010\n010")
+  end
+
   describe '#to_s' do
     it 'returns a string of 0s and 1s representing dead and alive cells' do
-      expect(described_class.new("010\n010\n010").to_s).to eql "010\n010\n010"
+      expect(subject.to_s).to eql "010\n010\n010"
+    end
+  end
+
+  describe '#next' do
+    it 'returns a new world with evolved cells' do
+      expect(subject.next.to_s).to eql "000\n111\n000"
     end
   end
 end
